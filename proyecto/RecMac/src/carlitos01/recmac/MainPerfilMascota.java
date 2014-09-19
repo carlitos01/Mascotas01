bfc3afc47ff6f30b4cc124ad2386b9569b05bb19
carlitos01 <carlitosabril01@gmail.com>
2014-09-19 00:09:16 -0500

    Creación del perfil de la mascota

---
+++ proyecto/RecMac/src/carlitos01/recmac/MainPerfilMascota.java
@@ -0,0 +1,13 @@
+package carlitos01.recmac;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainPerfilMascota extends Activity {
+
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.perfil_mascota);
+    }
+}
